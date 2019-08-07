15e61579f7c38a2ec670808d9f8e27c5b7042584
Eric Thomas <bluedevils027@gmail.com>
2019-08-07 16:08:29 -0400

    Added cors requirement

--- frontend/auxygen/src/app/app-constants.ts
+++ frontend/auxygen/src/app/app-constants.ts
@@ -1,8 +1,8 @@
 export class AppConstants {
     public static get localhost(): string {
         // For vm
-        return 'http://192.168.1.24:4200';
+        // return 'http://192.168.1.24:4200';
         // For same machine
-        // return 'http://localhost:4200';
+        return 'http://localhost:4200';
     }
 }
