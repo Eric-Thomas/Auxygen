ca6be369ae88ab3317de46ddde2fc62dd322062c
Eric Thomas <bluedevils027@gmail.com>
2019-08-17 13:57:49 -0400

    Changed ip in app constants

--- frontend/auxygen/src/app/app-constants.ts
+++ frontend/auxygen/src/app/app-constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
     public static get localhost(): string {
         // For vm
-        return 'http://192.168.1.24:4200';
+        return 'http://192.168.0.27:4200';
         // For same machine
         // return 'http://localhost:4200';
     }
